6b7779b8dd750cd7473b1972574a30a7f0f6cc60
Florent <pelletier.ft1@gmail.com>
2021-01-14 09:40:26 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ fr.bulletinfini/fr.bulletinfini/Classes/ShellViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Bulletinfini;
+using fr.bulletinfini.Vues;
+using MahApps.Metro.IconPacks;
+using System;
+using System.Collections.ObjectModel;
+
+namespace fr.bulletinfini.Classes
+{
+    public class ShellViewModel : Prism.Mvvm.BindableBase
+    {
+        private static readonly ObservableCollection<MenuItem> AppMenu = new ObservableCollection<MenuItem>();
+        private static readonly ObservableCollection<MenuItem> AppOptionsMenu = new ObservableCollection<MenuItem>();
+
+        public ObservableCollection<MenuItem> Menu => AppMenu;
+
+        public ObservableCollection<MenuItem> OptionsMenu => AppOptionsMenu;
+
+        public ShellViewModel()
+        {
+            // Build the menus
+            this.Menu.Add(new MenuItem()
+            {
+                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.HomeSolid },
+                Label = "Accueil",
+                NavigationType = typeof(WAccueil),
+                NavigationDestination = new Uri("Vues/WAccueil.xaml", UriKind.RelativeOrAbsolute)
+            });
+            this.Menu.Add(new MenuItem()
+            {
+                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.UserGraduateSolid },
+                Label = "Promotion",
+                NavigationType = typeof(WPromo),
+                NavigationDestination = new Uri("Vues/WPromo.xaml", UriKind.RelativeOrAbsolute)
+            });
+            this.Menu.Add(new MenuItem() 
+            {
+                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.ChalkboardTeacherSolid },
+                Label = "Awesome",
+                NavigationType = typeof(WMatiere),
+                NavigationDestination = new Uri("Vues/WMatiere.xaml", UriKind.RelativeOrAbsolute)
+            });
+            this.Menu.Add(new MenuItem()
+            {
+                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.MarkerSolid },
+                Label = "User",
+                NavigationType = typeof(WNote),
+                NavigationDestination = new Uri("Vues/WNote.xaml", UriKind.RelativeOrAbsolute)
+            });
+       
+
+            /* this.OptionsMenu.Add(new MenuItem()
+             {
+                 Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.CogsSolid },
+                 Label = "Settings",
+                 NavigationType = typeof(SettingsPage),
+                 NavigationDestination = new Uri("Views/SettingsPage.xaml", UriKind.RelativeOrAbsolute)
+             });
+             this.OptionsMenu.Add(new MenuItem()
+             {
+                 Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.InfoCircleSolid },
+                 Label = "About",
+                 NavigationType = typeof(AboutPage),
+                 NavigationDestination = new Uri("Views/AboutPage.xaml", UriKind.RelativeOrAbsolute)
+             });*/
+        }
+    }
+}
--- fr.bulletinfini/fr.bulletinfini/MainWindow.xaml.cs
+++ fr.bulletinfini/fr.bulletinfini/MainWindow.xaml.cs
@@ -1,13 +1,14 @@
-﻿using Bulletinfini;
-using fr.bulletinfini.Classes;
-using fr.bulletinfini.Vues;
+﻿using fr.bulletinfini.Classes;
 using MahApps.Metro.Controls;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.IO;
 using Microsoft.Win32;
+
+using System.Windows.Navigation;
+using MenuItem = fr.bulletinfini.Classes.MenuItem;
 
 namespace fr.bulletinfini
 {
@@ -16,21 +17,22 @@ namespace fr.bulletinfini
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private readonly NavigationServiceEx navigationServiceEx;
+
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            this.navigationServiceEx = new NavigationServiceEx();
+            this.navigationServiceEx.Navigated += this.NavigationServiceEx_OnNavigated;
+            this.HamburgerMenuControl.Content = this.navigationServiceEx.Frame;
+
+            // Navigate to the home page.
+            this.Loaded += (sender, args) => this.navigationServiceEx.Navigate(new Uri("Vues/Home.xaml", UriKind.RelativeOrAbsolute));
         }
 
-        private void OnClick(object sender, RoutedEventArgs e)
+        private void HamburgerMenuControl_OnItemInvoked(object sender, HamburgerMenuItemInvokedEventArgs e)
         {
-            Microsoft.Win32.OpenFileDialog Browser = new Microsoft.Win32.OpenFileDialog();
-            Browser.Title = "Selectionner votre fichier de données";
-            Browser.DefaultExt = ".json";
-            Browser.Filter = "Json Files (*.json)|*.json";
-            Browser.FilterIndex = 1;
-
-            Nullable<bool> result = Browser.ShowDialog();
-            if (result == true)
+            if (e.InvokedItem is MenuItem menuItem && menuItem.IsNavigation)
             {
                 try
                 {
@@ -45,10 +47,11 @@ namespace fr.bulletinfini
                 {
                     MessageBox.Show("Le fichier fourni est corompu, \n Merci de forunir un Fichier valide", "Erreur", MessageBoxButton.OK);
                 }
+                this.navigationServiceEx.Navigate(menuItem.NavigationDestination);
             }
         }
 
-        private void Click_GenerateNewFile(object sender, RoutedEventArgs e)
+        private void NavigationServiceEx_OnNavigated(object sender, NavigationEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Title = "Séléctionner l'emplacement du fichier";
@@ -64,6 +67,15 @@ namespace fr.bulletinfini
             }
             test();
         }
+            // select the menu item
+            this.HamburgerMenuControl.SelectedItem = this.HamburgerMenuControl
+                                                         .Items
+                                                         .OfType<MenuItem>()
+                                                         .FirstOrDefault(x => x.NavigationDestination == e.Uri);
+            this.HamburgerMenuControl.SelectedOptionsItem = this.HamburgerMenuControl
+                                                                .OptionsItems
+                                                                .OfType<MenuItem>()
+                                                                .FirstOrDefault(x => x.NavigationDestination == e.Uri);
 
         private void test()
         {
@@ -71,6 +83,15 @@ namespace fr.bulletinfini
             Promotion p1 = new Promotion();
             p1.Intitule = "yguy";
             p1.Date = "2020";
+            // or when using the NavigationType on menu item
+            // this.HamburgerMenuControl.SelectedItem = this.HamburgerMenuControl
+            //                                              .Items
+            //                                              .OfType<MenuItem>()
+            //                                              .FirstOrDefault(x => x.NavigationType == e.Content?.GetType());
+            // this.HamburgerMenuControl.SelectedOptionsItem = this.HamburgerMenuControl
+            //                                                     .OptionsItems
+            //                                                     .OfType<MenuItem>()
+            //                                                     .FirstOrDefault(x => x.NavigationType == e.Content?.GetType());
 
             Eleve e1 = new Eleve();
             e1.Nom = "Damien";
@@ -97,6 +118,8 @@ namespace fr.bulletinfini
 
 
             System.IO.File.WriteAllText(@"C:\Users\Damien\Desktop\test1.json", JsonConvert.SerializeObject(promol, Formatting.Indented, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects }));
+            // update back button
+            this.GoBackButton.Visibility = this.navigationServiceEx.CanGoBack ? Visibility.Visible : Visibility.Collapsed;
         }
 
         private void WindowLoad(object sender, RoutedEventArgs e)
@@ -116,6 +139,11 @@ namespace fr.bulletinfini
         private void GenerateConfigurationFile(String src)
         {
             System.IO.File.WriteAllText(@Directory.GetCurrentDirectory() + "\\config.txt", src);
+        }
+
+        private void GoBack_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.navigationServiceEx.GoBack();
         }
     }
 }
